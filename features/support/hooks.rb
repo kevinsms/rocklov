5bd2d499ff8c62b1faa34a3947b20a8abe9ae53c
kevinsms <kevin.mss@hotmail.com>
2023-09-04 22:38:50 -0300

    Adicionando variaveis de ambiente por YML

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -4,5 +4,14 @@ Before do
   @dash_page = DashPage.new
   @signup_page = SignupPage.new
   @login_page = LoginPage.new
-  page.current_window.resize_to(1440,900)
+  page.current_window.resize_to(1440, 900)
+end
+
+After do
+  temp_shot = page.save_screenshot("logs/temp_screenshot.png")
+  Allure.add_attachment(
+    name: "Screenshot",
+    type: Allure::ContentType::PNG,
+    source: File.open(temp_shot),
+  )
 end
